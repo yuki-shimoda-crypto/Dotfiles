d4c134f4a91a0a206547cddf4f06c1a1fa5fca01
yuki-shimoda-crypto <77478109+yuki-shimoda-crypto@users.noreply.github.com>
2023-03-21 13:53:16 +0900

    [add] common.bashrc fedora.bashrc mac.bashrc ubuntu.bashrc wsl_ubuntu.bashrc [fix] restore.sh

--- restore.sh
+++ restore.sh
@@ -3,7 +3,14 @@ ln -s $(pwd)/.bashrc ~/.bashrc
 ln -s $(pwd)/.gitconfig ~/.gitconfig
 ln -s $(pwd)/.vimrc ~/.vimrc
 ln -s $(pwd)/.zshrc ~/.zshrc
-ln -s $(pwd)/.bashrc.d ~/.bashrc.d
 
 mkdir -p ~/.ssh
 ln -s $(pwd)/.ssh/config ~/.ssh/config
+
+# make .bashrc.d
+mkdir -p ~/.bashrc.d
+ln -s $(pwd)/.bashrc.d/common.bashrc		~/.bashrc.d/common.bashrc
+ln -s $(pwd)/.bashrc.d/fedora.bashrc		~/.bashrc.d/fedora.bashrc
+ln -s $(pwd)/.bashrc.d/mac.bashrc			~/.bashrc.d/mac.bashrc
+ln -s $(pwd)/.bashrc.d/ubuntu.bashrc		~/.bashrc.d/ubuntu.bashrc
+ln -s $(pwd)/.bashrc.d/wsl_ubuntu.bashrc	~/.bashrc.d/wsl_ubuntu.bashrc
